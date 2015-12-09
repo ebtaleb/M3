5f2743e3c67da3565d715202b32e7bb07caf11c3
E BOUTALEB <kagekyio@gmail.com>
2015-12-09 16:25:41 +0100

    Add d3.js webview

--- app/src/main/java/com/upmc/m3/GraphActivity.java
+++ app/src/main/java/com/upmc/m3/GraphActivity.java
@@ -1,19 +1,66 @@
 package com.upmc.m3;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.google.gson.Gson;
+import com.upmc.m3.domain.DataPoint;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class GraphActivity extends AppCompatActivity {
+
+    @Bind(R.id.webview)
+    WebView webview;
+
+    public class WebAppInterface {
+        private Context context;
+
+        public WebAppInterface(Context context) {
+            this.context = context;
+        }
+
+        @JavascriptInterface
+        public String loadData() {
+            return createDataSet();
+        }
+
+        private final String createDataSet() {
+            final Random rand = new Random(System.currentTimeMillis());
+            final String[] x = new String[] {
+                    "A", "B", "C", "D", "E", "F",
+                    "G", "H", "I", "J", "K", "L",
+                    "M", "N", "O", "P", "Q", "R",
+                    "S", "T", "U", "V", "W", "X",
+                    "Y", "Z"};
+            final List<DataPoint> set = new ArrayList<DataPoint>();
+            for (int i = 0; i < x.length ; i++) {
+                set.add( new DataPoint(x[i], rand.nextFloat()));
+            }
+            final DataPoint[] pts = set.toArray( new DataPoint[]{} );
+            return new Gson().toJson(pts, DataPoint[].class );
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_child);
-
-        // my_child_toolbar is defined in the layout file
-        Toolbar myChildToolbar = (Toolbar) findViewById(R.id.my_child_toolbar);
+        ButterKnife.bind(this);
+        Toolbar myChildToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         //setSupportActionBar(myChildToolbar);
 
         // Get a support ActionBar corresponding to this toolbar
@@ -21,5 +68,23 @@ public class GraphActivity extends AppCompatActivity {
 
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        final WebSettings ws = webview.getSettings();
+        ws.setJavaScriptEnabled(true);
+        //ws.setPluginState(WebSettings.PluginState.ON);
+        ws.setAllowFileAccess(true);
+        ws.setDomStorageEnabled(true);
+        ws.setAllowContentAccess(true);
+        ws.setAllowFileAccessFromFileURLs(true);
+        ws.setAllowUniversalAccessFromFileURLs(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.setWebChromeClient(new WebChromeClient());
+        webview.addJavascriptInterface( new WebAppInterface( this ), "Android");
+        webview.loadUrl("file:///android_asset/main.html");
     }
 }
