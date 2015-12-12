446a311dfbf0818f24f3c7e8c1d2b26b5ac24097
E BOUTALEB <kagekyio@gmail.com>
2015-12-12 18:30:16 +0100

    Graph menu, working d3 mindmap

---
+++ app/src/main/assets/test.mm
@@ -0,0 +1,41 @@
+<map version="1.0.1">
+             <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1400771016814" ID="ID_1425716387" MODIFIED="1400771026053" TEXT="Mindmap">
+                                                                               <node CREATED="1400771027265" ID="ID_846682469" MODIFIED="1400771029816" POSITION="right" TEXT="From Freemind">
+                                                                                                                                                                              <node CREATED="1400771032900" ID="ID_827918588" MODIFIED="1400771037791" TEXT="Stored as .mm"/>
+                                                                                                                                                                                                                                                            <node CREATED="1400771038772" ID="ID_1158944789" MODIFIED="1400771044688" TEXT="Really its an xml file"/>
+                                                                                                                                                                                                                                                                                                                                           </node>
+<node CREATED="1400771048651" ID="ID_895287109" MODIFIED="1400771056767" POSITION="left" TEXT="Just the basics">
+                                                                                              <node CREATED="1400771105555" ID="ID_420969231" MODIFIED="1400771114341" TEXT="bold">
+                                                                                                                                                                            <font BOLD="true" NAME="SansSerif" SIZE="12"/>
+                                                                                                                                                                                                                    </node>
+<node CREATED="1400771107260" ID="ID_1006485332" MODIFIED="1400771115614" TEXT="italic">
+                                                                               <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+                                                                                                                         </node>
+<node CREATED="1400771116050" ID="ID_660540640" MODIFIED="1400771123415" TEXT="Cloud">
+                                                                              <cloud/>
+                                                                              </node>
+</node>
+<node CREATED="1400771057531" ID="ID_1127763371" MODIFIED="1400771066263" POSITION="left" TEXT="Maybe a few icons">
+                                                                                               <node CREATED="1400771067555" ID="ID_850944552" MODIFIED="1400771073088" TEXT="1">
+                                                                                                                                                                             <icon BUILTIN="full-1"/>
+                                                                                                                                                                                           </node>
+<node CREATED="1400771074490" ID="ID_1208594920" MODIFIED="1400771080110" TEXT="2">
+                                                                               <icon BUILTIN="full-2"/>
+                                                                                             </node>
+<node CREATED="1400771076659" ID="ID_1278721112" MODIFIED="1400771082280" TEXT="3">
+                                                                               <icon BUILTIN="full-3"/>
+                                                                                             </node>
+<node CREATED="1400771089739" ID="ID_544883255" MODIFIED="1400771093502" TEXT="idea">
+                                                                              <icon BUILTIN="idea"/>
+                                                                                            </node>
+</node>
+<node CREATED="1400771143034" ID="ID_1094518542" MODIFIED="1400771185233" POSITION="right" STYLE="fork" TEXT="Large text">
+                                                                                                             <font NAME="SansSerif" SIZE="19"/>
+                                                                                                                                         </node>
+<node CREATED="1400771186530" ID="ID_25862265" MODIFIED="1400771223153" POSITION="right" STYLE="fork" TEXT="Outline Formats">
+                                                                                                           <node CREATED="1400771197723" ID="ID_514062041" MODIFIED="1400771223153" STYLE="bubble" TEXT="Bubble"/>
+                                                                                                                                                                                                        <node CREATED="1400771199834" ID="ID_1161671026" MODIFIED="1400771223153" STYLE="fork" TEXT="Fork"/>
+                                                                                                                                                                                                                                                                                                    </node>
+</node>
+</map>
--- app/src/main/java/com/upmc/m3/GraphActivity.java
+++ app/src/main/java/com/upmc/m3/GraphActivity.java
@@ -1,22 +1,22 @@
 package com.upmc.m3;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
-import com.google.gson.Gson;
-import com.upmc.m3.domain.DataPoint;
+import android.widget.EditText;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -35,23 +35,7 @@ public class GraphActivity extends AppCompatActivity {
 
         @JavascriptInterface
         public String loadData() {
-            return createDataSet();
-        }
-
-        private final String createDataSet() {
-            final Random rand = new Random(System.currentTimeMillis());
-            final String[] x = new String[] {
-                    "A", "B", "C", "D", "E", "F",
-                    "G", "H", "I", "J", "K", "L",
-                    "M", "N", "O", "P", "Q", "R",
-                    "S", "T", "U", "V", "W", "X",
-                    "Y", "Z"};
-            final List<DataPoint> set = new ArrayList<DataPoint>();
-            for (int i = 0; i < x.length ; i++) {
-                set.add( new DataPoint(x[i], rand.nextFloat()));
-            }
-            final DataPoint[] pts = set.toArray( new DataPoint[]{} );
-            return new Gson().toJson(pts, DataPoint[].class );
+            return "";
         }
     }
 
@@ -60,14 +44,72 @@ public class GraphActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_child);
         ButterKnife.bind(this);
-        Toolbar myChildToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        //Toolbar myChildToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         //setSupportActionBar(myChildToolbar);
 
         // Get a support ActionBar corresponding to this toolbar
-        ActionBar ab = getSupportActionBar();
+        //ActionBar ab = getSupportActionBar();
 
         // Enable the Up button
-        ab.setDisplayHomeAsUpEnabled(true);
+        //ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void setNewNodeName() {
+
+        String name = "";
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("New node name");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String node_n = input.getText().toString();
+                webview.loadUrl("javascript:alert('" + node_n + "')");
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.export_action:
+                new AlertDialog.Builder(this)
+                        .setTitle("Delete entry")
+                        .setMessage("Are you sure you want to delete this entry?")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .show();
+                return true;
+            case R.id.reset_action:
+                webview.loadUrl("javascript:createNew()");
+                return true;
+            case R.id.new_node_action:
+                setNewNodeName();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_graph, menu);
+        return true;
     }
 
     @Override
@@ -76,7 +118,6 @@ public class GraphActivity extends AppCompatActivity {
 
         final WebSettings ws = webview.getSettings();
         ws.setJavaScriptEnabled(true);
-        //ws.setPluginState(WebSettings.PluginState.ON);
         ws.setAllowFileAccess(true);
         ws.setDomStorageEnabled(true);
         ws.setAllowContentAccess(true);
@@ -84,6 +125,8 @@ public class GraphActivity extends AppCompatActivity {
         ws.setAllowUniversalAccessFromFileURLs(true);
         webview.setWebViewClient(new WebViewClient());
         webview.setWebChromeClient(new WebChromeClient());
+        webview.getSettings().setLoadWithOverviewMode(true);
+        webview.getSettings().setUseWideViewPort(true);
         webview.addJavascriptInterface( new WebAppInterface( this ), "Android");
         webview.loadUrl("file:///android_asset/main.html");
     }
